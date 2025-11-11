668dacb9151f997c95f6c336e41d28b7eda5e471
MorarLorena27 <lorenamorar2006@gmail.com>
2025-11-11 13:08:30 +0200

    16

--- laborator_5_mi_1/functii_studenti.py
+++ laborator_5_mi_1/functii_studenti.py
@@ -1,12 +1,11 @@
-if __name__ == '__main__':
-    def timp_in_format(secunde: int) -> str:
-        if secunde<0:
-            return "Eroare: numărul de secunde trebuie să fie nenegativ."
-        ore=secunde//3600
-        minute=(secunde%3600)//60
-        secunde_ramase=secunde%60
-        hh=f"{ore:02d}"
-        mm=f"{minute:02d}"
-        ss=f"{secunde_ramase:02d}"
-        timp_format=f"{hh}:{mm}:{ss}"
-        return f"{secunde} secunde inseamna {timp_format}."
+def timp_in_format(secunde: int) -> str:
+    if secunde<0:
+        return "Eroare: numărul de secunde trebuie să fie nenegativ."
+    ore=secunde//3600
+    minute=(secunde%3600)//60
+    secunde_ramase=secunde%60
+    hh=f"{ore:02d}"
+    mm=f"{minute:02d}"
+    ss=f"{secunde_ramase:02d}"
+    timp_format=f"{hh}:{mm}:{ss}"
+    return f"{secunde} secunde inseamna {timp_format}."
