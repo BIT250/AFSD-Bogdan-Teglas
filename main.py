1f35aa9ca836899f46bd50ea7598578f1370949a
Bogdan <bogdanioanteglas@yahoo.com>
2025-11-11 15:23:49 +0200

    Revert "Merge branch 'main' of https://github.com/BIT250/AFSD-Bogdan-Teglas"
    This reverts commit f68cff30d7f27f65b7edb727e37636eb278a4901, reversing
    changes made to d13a714e1ed76d148e4ca98b5fe94947e9b5267d.

--- main.py
+++ main.py
@@ -1,36 +1,20 @@
-# metoda bubble sort
-
-# o lista de numere intregi aleatoare
-# cum o sortam prin bubble sort
-
-# 1. definim functia bubble sort
-#       - parametru: lista
-# 2. parcurgem lista de la primul element pana la penultimul
-# 3. comparam elementul curent cu urmatorul
-#      - daca elem. curent > urmatorul
-#          - le interschimbam
-#       - altfel
-#          - trecem la urmatorul element
-# la fiecare iteratie, ultimul element este la locul lui
-# ne oprim cand nu mai sunt schimbari
-lista = [25, 12, 22, 11,34,64, 90]
+lista = [1, 5, 3, 7, 2, 9, 4]
 
 def bubble_sort(lista):
-	schimbari = True
-	while schimbari:
+	interschimbare = True
+	while interschimbare:
+		interschimbare = False
 		i = 0
-		schimbari = False
-		# i e pozitia curenta
-		# lista[i] e elementul curent
-		# i+1 pozitia urmatoare
-		# lista[i+1] e elementul urmator
 		while i < len(lista) - 1:
-			if lista[i] > lista[i+1]:
-				# interschimbam
-				lista[i], lista[i+1] = lista[i+1], lista[i]
-				schimbari = True
+			# i e pozitia curenta
+			# i + 1 e pozitia urmatoare
+			# valoarea curenta: lista[i]
+			# valoarea urmatoare: lista[i + 1]
+			if lista[i] > lista[i + 1]:
+				interschimbare = True
+				lista[i], lista[i + 1] = lista[i + 1], lista[i]
 			i += 1
-
 	print(lista)
+	return lista
 
 bubble_sort(lista)
